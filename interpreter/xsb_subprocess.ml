975c341aea322435e21b560db9e526ce9ed116b3
Michael Scheer <mgscheer@gmail.com>
2013-06-05 01:26:39 -0400

    automated sending asserts and queries

--- interpreter/xsb_subprocess.ml
+++ interpreter/xsb_subprocess.ml
@@ -9,8 +9,6 @@ let xsb_in, to_xsb = Unix.pipe();;
 "The first component of the result is opened for reading, that's the exit to the pipe. 
 The second component is opened for writing, that's the entrance to the pipe."
 *)
-(*let infromx_descr, child_stdout = Unix.pipe();;
-let child_stdin, outtox_descr = Unix.pipe();;*)
 
 (* Convert the file descriptors created to channels *)
 let xin_channel = in_channel_of_descr from_xsb;;
@@ -27,23 +25,67 @@ First: new stdin (OUT TO X)
 Second: new stdout (IN FROM X)
  *)
 
-Unix.create_process "xsb" [|"xsb"|] xsb_in xsb_out Unix.stderr;;
+Unix.create_process "/Applications/XSB/bin/xsb" [|"xsb"|] xsb_in xsb_out Unix.stderr;;
 Unix.sleep 1;;
 
 print_endline "Process created.";;
 
-(* trying to use streams for ease of use *)
+(*let send_query str out_ch in_ch =
+	output_string out_ch (str ^ "\n");
+	flush out_ch;
+	let answer = ref "" in
+	let next_str = ref "" in
+	while (!next_str <> "yes" && !next_str <> "no") do
+		(*print_endline ("next_str " ^ !next_str);*)
+		next_str := input_line in_ch;
+		answer := (!answer ^ "\n" ^ !next_str);
+	done;
+	String.trim !answer;;*)
 
-(* From http://ocaml.org/tutorials/streams.html *)
 
-let line_stream_of_channel channel =
-  Stream.from
-    (fun _ ->
-       try Some (input_line channel) with End_of_file -> None);;
+let send_assert str out_ch in_ch =
+	output_string out_ch (str ^ "\n");
+	flush out_ch;
+	let answer = ref "" in
+	let next_str = ref "" in
+	while (!next_str <> "yes" && !next_str <> "no") do
+		next_str := input_line in_ch;
+		answer := (!answer ^ "\n" ^ !next_str);
+	done;
+	String.trim !answer;;
 
-(* Stream for input from xsb! [[forgetting about streams for now]]*)
-(*let xin_stream = line_stream_of_channel xin_channel;;*)
+print_endline (send_assert "assert(p(1))." xout_channel xin_channel);;
+flush Pervasives.stdout;;
 
+print_endline (send_assert "assert(p(2))." xout_channel xin_channel);;
+flush Pervasives.stdout;;
+
+let ends_with str1 str2 = 
+	if String.length str2 > String.length str1
+	then false
+	else String.sub str1 ((String.length str1) - (String.length str2)) (String.length str2) = str2;;
+
+let send_query str out_ch in_ch =
+	output_string out_ch (str ^ "\n");
+	flush out_ch;
+	let answer = ref (input_line in_ch) in
+	let next_str = ref "" in
+	while not (ends_with !next_str "yes") && not (ends_with !next_str "no") do
+		output_string out_ch ";\n";
+		flush out_ch;
+		next_str := input_line in_ch;
+		answer := (!answer ^ "\n" ^ !next_str);
+	done;
+	String.trim !answer;;
+
+print_endline (send_query "p(X)." xout_channel xin_channel);;
+flush Pervasives.stdout;;
+
+(*
+
+			if !next_str == "" then
+			output_string out_ch ";\n";
+			flush out_ch;
 output_string xout_channel "assert(p(123)).\n";;
 flush xout_channel;;
 
@@ -72,13 +114,12 @@ output_string xout_channel "writeln(hi).\n";;
 flush xout_channel;;
 
 (* prompt? *)
-print_endline (input_line xin_channel);;
+(*print_endline (input_line xin_channel);;
 flush Pervasives.stdout;
 (* yes *)
 print_endline (input_line xin_channel);;
 flush Pervasives.stdout;
 
-(* can we do it again? *)
 (*output_string xout_channel "p(X), writeln(X).\n";;*)
 output_string xout_channel "p(X).\n";;
 flush xout_channel;;
@@ -87,15 +128,30 @@ flush xout_channel;;
    because the bindings hold at the end of X=...  
    input_line will block forever in this case. *)
 
-output_string xout_channel ";\n";;
-flush xout_channel;;
+(*try print_endline (input_line xin_channel) with End_of_file -> ();;*)
+
+print_endline (input_line xin_channel);;
+flush Pervasives.stdout;
 
 output_string xout_channel ";\n";;
 flush xout_channel;;
+
+print_endline (input_line xin_channel);;
+flush Pervasives.stdout;
+
+output_string xout_channel ";\n";;
+flush xout_channel;;
+
+print_endline (input_line xin_channel);;
+flush Pervasives.stdout;
+
+print_endline (input_line xin_channel);;
+flush Pervasives.stdout;
+
 
 
 (* prompt? *)
-print_endline (input_line xin_channel);;
+(*print_endline (input_line xin_channel);;
 flush Pervasives.stdout;
 (* yes *)
 print_endline (input_line xin_channel);;
@@ -103,40 +159,8 @@ flush Pervasives.stdout;
 print_endline (input_line xin_channel);;
 flush Pervasives.stdout;
 print_endline (input_line xin_channel);;
-flush Pervasives.stdout;
-
-(*without this next command the program runs fine but with it it holds. this means xsb isn't writing to the stream in response to writeln(hi).*)
-
-
-(*output_string xout_channel "p(X).\n";;
-flush xout_channel;;
-
-(* ? *)
-print_endline (input_line xin_channel);;
-flush Pervasives.stdout;
+flush Pervasives.stdout;*)
 *)
-
-
-(*print_endline (input_line xin_channel);;
-flush Pervasives.stdout;
 *)
-
-(*
-print_string (input_line xin_channel);;
-
-print_char (input_char xin_channel);;
-print_char (input_char xin_channel);;
-print_char (input_char xin_channel);;
-print_char (input_char xin_channel);;
-print_char (input_char xin_channel);;
-print_char (input_char xin_channel);;
-print_char (input_char xin_channel);;
-print_char (input_char xin_channel);;
-print_char (input_char xin_channel);;
-print_char (input_char xin_channel);;
-*)
-
-
-Unix.sleep 1;;
 output_string xout_channel "halt.\n";;
 flush xout_channel;;
