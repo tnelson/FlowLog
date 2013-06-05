21face8e981d60a0cc62e4b66c7968f24d9ce23c
Michael Scheer <mgscheer@gmail.com>
2013-06-05 11:32:53 -0400

    grouped file into a module

--- interpreter/xsb_subprocess.ml
+++ interpreter/xsb_subprocess.ml
@@ -3,30 +3,26 @@ open Unix;;
 open Printf;;
 
 
-let from_xsb, xsb_out = Unix.pipe();;
-let xsb_in, to_xsb = Unix.pipe();; 
+module Xsb = struct
+	
+(* creates a pair channels for talking to xsb, starts xsb, and returns the channels *)
+let start_xsb () : out_channel * in_channel =
+	let from_xsb, xsb_out = Unix.pipe() in
+	let xsb_in, to_xsb = Unix.pipe() in
+	(* Convert the file descriptors created to channels *)
+	let xin_channel = in_channel_of_descr from_xsb in
+	let xout_channel = out_channel_of_descr to_xsb in
+	(* in text mode *)
+	set_binary_mode_out xout_channel false;
+	set_binary_mode_in xin_channel false;
+	(* start xsb *)
+	Unix.create_process "/Applications/XSB/bin/xsb" [|"xsb"|] xsb_in xsb_out Unix.stderr;
+	(xout_channel, xin_channel);;
+	
 
-(* Create a pipe for the child 
-"The first component of the result is opened for reading, that's the exit to the pipe. 
-The second component is opened for writing, that's the entrance to the pipe."
-*)
-
-(* Convert the file descriptors created to channels *)
-let xin_channel = in_channel_of_descr from_xsb;;
-(*print_endline "Child's stdout channel created. Becomes an in channel.";;*)
-let xout_channel = out_channel_of_descr to_xsb;;  
-(*print_endline "Child's stdin channel created. Becomes an out channel.";;*)
-
-(* in text mode *)
-set_binary_mode_out xout_channel false;;
-set_binary_mode_in xin_channel false;;
-
-Unix.create_process "/Applications/XSB/bin/xsb" [|"xsb"|] xsb_in xsb_out Unix.stderr;;
-
-(*print_endline "Process created.";;*)
-
-(* This takes in a string command (not query, this doesn't deal with the semicolons) and two channels (to and from xsb). It writes the command to xsb and returns the resulting text.*)
-let send_assert str out_ch in_ch =
+(* This takes in a string command (not query, this doesn't deal with the semicolons) and two channels (to and from xsb).
+It writes the command to xsb and returns the resulting text.*)
+let send_assert (str : string) (out_ch : out_channel) (in_ch : in_channel) : string =
 	output_string out_ch (str ^ "\n");
 	flush out_ch;
 	let answer = ref "" in
@@ -37,23 +33,21 @@ let send_assert str out_ch in_ch =
 	done;
 	String.trim !answer;;
 
-send_assert "assert(p(1))." xout_channel xin_channel;;
-
-send_assert "assert(p(2))." xout_channel xin_channel;;
 
 (* True if string str1 ends with string str2 *)
-let ends_with str1 str2 = 
+let ends_with (str1 : string) (str2 : string) : bool = 
 	if String.length str2 > String.length str1
 	then false
 	else String.sub str1 ((String.length str1) - (String.length str2)) (String.length str2) = str2;;
 
 (* Removes str2 from the end of str1 if its there, otherwise returns str1 *)
-let remove_from_end str1 str2 = 
+let remove_from_end (str1 : string) (str2 : string) : string = 
 	if ends_with str1 str2
 	then String.sub str1 0 ((String.length str1) - (String.length str2))
 	else str1;; 
 
-let rec group alist num = 
+(* groups elements of alist into lists of size num except possibly the first one *)
+let rec group (alist : 'a list) (num : int) : ('a list) list = 
 	match alist with
 	| [] -> [];
 	| f :: r -> match group r num with
@@ -64,7 +58,7 @@ let rec group alist num =
 
 (* Takes a string query (thing with semicolon answers), the number of variables involved, and the in and out chanels.
  It writes the query to xsb and returns a list of lists with all of the results. *)
-let send_query str num_vars out_ch in_ch =
+let send_query (str : string) (num_vars : int) (out_ch : out_channel) (in_ch : in_channel) : (string list) list =
 	output_string out_ch (str ^ "\n");
 	flush out_ch;
 	let _ = input_line in_ch in
@@ -81,6 +75,20 @@ let send_query str num_vars out_ch in_ch =
 	done;
 	group (List.rev !answer) num_vars;;
 
+let halt_xsb (out_ch : out_channel) : unit = 
+	output_string out_ch "halt.\n";
+	flush out_ch;;
+
+end
+
+(* examples *)
+open Xsb;;
+let xout_channel, xin_channel = start_xsb ();;
+
+send_assert "assert(p(1))." xout_channel xin_channel;;
+
+send_assert "assert(p(2))." xout_channel xin_channel;;
+
 List.iter (List.iter (printf "%s ")) (send_query "p(X)." 1 xout_channel xin_channel);;
 flush Pervasives.stdout;;
 
@@ -93,95 +101,9 @@ send_assert "[mac_learning]." xout_channel xin_channel;;
 
 send_assert "assert(learned(1,5,4))." xout_channel xin_channel;;
 
-
-(*print_string "ready for the big query";;
-flush Pervasives.stdout;;*)
-
 List.iter (List.iter (printf "%s ")) (send_query "emit(1,2,3,4,5,6,7,8, LocSw2, LocPt2, DlSrc2, DlDst2, DlTyp2, NwSrc2, NwDst2, NwProto2)." 8 xout_channel xin_channel);;
 flush Pervasives.stdout;;
 
 
-
-(*
-
-			if !next_str == "" then
-			output_string out_ch ";\n";
-			flush out_ch;
-output_string xout_channel "assert(p(123)).\n";;
-flush xout_channel;;
-
-(* prompt? *)
-print_endline (input_line xin_channel);;
-flush Pervasives.stdout;
-(* yes *)
-print_endline (input_line xin_channel);;
-flush Pervasives.stdout;
-
-
-
-(* can we do it again? *)
-output_string xout_channel "assert(p(456)).\n";;
-flush xout_channel;;
-
-(* prompt? *)
-print_endline (input_line xin_channel);;
-flush Pervasives.stdout;
-(* yes *)
-print_endline (input_line xin_channel);;
-flush Pervasives.stdout;
-
-(* can we do it again? *)
-output_string xout_channel "writeln(hi).\n";;
-flush xout_channel;;
-
-(* prompt? *)
-(*print_endline (input_line xin_channel);;
-flush Pervasives.stdout;
-(* yes *)
-print_endline (input_line xin_channel);;
-flush Pervasives.stdout;
-
-(*output_string xout_channel "p(X), writeln(X).\n";;*)
-output_string xout_channel "p(X).\n";;
-flush xout_channel;;
-
-(* need to do something different in the end. it's waiting for a semicolon
-   because the bindings hold at the end of X=...  
-   input_line will block forever in this case. *)
-
-(*try print_endline (input_line xin_channel) with End_of_file -> ();;*)
-
-print_endline (input_line xin_channel);;
-flush Pervasives.stdout;
-
-output_string xout_channel ";\n";;
-flush xout_channel;;
-
-print_endline (input_line xin_channel);;
-flush Pervasives.stdout;
-
-output_string xout_channel ";\n";;
-flush xout_channel;;
-
-print_endline (input_line xin_channel);;
-flush Pervasives.stdout;
-
-print_endline (input_line xin_channel);;
-flush Pervasives.stdout;
-
-
-
-(* prompt? *)
-(*print_endline (input_line xin_channel);;
-flush Pervasives.stdout;
-(* yes *)
-print_endline (input_line xin_channel);;
-flush Pervasives.stdout;
-print_endline (input_line xin_channel);;
-flush Pervasives.stdout;
-print_endline (input_line xin_channel);;
-flush Pervasives.stdout;*)
-*)
-*)
-output_string xout_channel "halt.\n";;
-flush xout_channel;;
+(* always close the channel at the end *)
+halt_xsb xout_channel
