b1bab670af35346b80c534b1978d55e19c23fdd8
Michael Scheer <mgscheer@gmail.com>
2013-06-04 21:02:27 -0400

    not sure whats wrong

--- interpreter/xsb_subprocess.ml
+++ interpreter/xsb_subprocess.ml
@@ -1,47 +1,34 @@
 #load "unix.cma";;
 open Unix;;
 
-(*match fork () with
-| 0 -> execv "/Applications/XSB/bin/xsb" [|"xsb"|];
-| pid -> exit 0;;*)
 
-
-
-(* create_process prog args new_stdin new_stdout new_stderr *)
-(* this line will make the parent's stdin = xsb's stdin, etc.
-   that is: we'd have to TYPE assert(p(1)). to get the "yes" back.
-Unix.create_process "xsb" [||] Unix.stdin Unix.stdout Unix.stderr;;
-   we want something different, right?
-*)
-
-(* none of this works below... can run the above and just type xsb commands though. *)
+let from_xsb, xsb_out = Unix.pipe();;
+let xsb_in, to_xsb = Unix.pipe();; 
 
 (* Create a pipe for the child 
 "The first component of the result is opened for reading, that's the exit to the pipe. 
 The second component is opened for writing, that's the entrance to the pipe."
 *)
-let infromx_descr, child_stdout = Unix.pipe();;
-let child_stdin, outtox_descr = Unix.pipe();;
+(*let infromx_descr, child_stdout = Unix.pipe();;
+let child_stdin, outtox_descr = Unix.pipe();;*)
 
 (* Convert the file descriptors created to channels *)
-let xin_channel = in_channel_of_descr infromx_descr;;
+let xin_channel = in_channel_of_descr from_xsb;;
 print_endline "Child's stdout channel created. Becomes an in channel.";;
-let xout_channel = out_channel_of_descr outtox_descr;;  
+let xout_channel = out_channel_of_descr to_xsb;;  
 print_endline "Child's stdin channel created. Becomes an out channel.";;
 
 (* in text mode *)
 set_binary_mode_out xout_channel false;;
 set_binary_mode_in xin_channel false;;
 
-Unix.sleep 1;;
-
 (* Use the pipe. Can keep stderr the same so XSB errors print in interpreter.
 First: new stdin (OUT TO X)
 Second: new stdout (IN FROM X)
  *)
 
-Unix.create_process "xsb" [|"xsb"|] child_stdin child_stdout Unix.stderr;;
-Unix.sleep 3;;
+Unix.create_process "/Applications/XSB/bin/xsb" [|"xsb"|] xsb_in xsb_out Unix.stderr;;
+Unix.sleep 1;;
 
 print_endline "Process created.";;
 
@@ -57,32 +44,18 @@ let line_stream_of_channel channel =
 (* Stream for input from xsb! *)
 let xin_stream = line_stream_of_channel xin_channel;;
 
-(*print_endline (Stream.next xin_stream);;*)
 
-print_endline "issuing halt command";;
+print_char (input_char xin_channel);;
+print_char (input_char xin_channel);;
+print_char (input_char xin_channel);;
+print_char (input_char xin_channel);;
+print_char (input_char xin_channel);;
 
-output_string xout_channel "halt.\n";;
+output_string xout_channel "writeln(hi).\\n";;
 flush xout_channel;;
 
-(* don't try to get anything from the channel after xsb closes
-print_char (input_char xin_channel);;
-print_char (input_char xin_channel);;
+(*without this next command the program runs fine but with it it holds. this means xsb isn't writing to the stream in response to writeln(hi).*)
+(*print_char (input_char xin_channel);;*)
 
-print_char (input_char xin_channel);;
-print_char (input_char xin_channel);;
-print_char (input_char xin_channel);;
-print_char (input_char xin_channel);;
-print_char (input_char xin_channel);;
-print_char (input_char xin_channel);;
-*)
-
-print_endline "done!";;
-
-(*
-No need to do this: the prompt will have the streams etc. defined.
-
-while true
-do
-print_string ""
-done;;
-*)
+output_string xout_channel "halt.\\n";;
+flush xout_channel;;
