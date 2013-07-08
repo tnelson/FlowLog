bba62d2b54c4d9ff54f96a0487ed71e841c3e0fa
Tim Nelson <tn@cs.wpi.edu>
2013-07-08 12:07:35 -0400

    partial thrift skeleton

--- interpreter/thrift/testBB.ml
+++ interpreter/thrift/testBB.ml
@@ -1,79 +1,37 @@
 (*
- Licensed to the Apache Software Foundation (ASF) under one
- or more contributor license agreements. See the NOTICE file
- distributed with this work for additional information
- regarding copyright ownership. The ASF licenses this file
- to you under the Apache License, Version 2.0 (the
- "License"); you may not use this file except in compliance
- with the License. You may obtain a copy of the License at
+  Modified from ocaml tutorial by Tim
 
-   http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing,
- software distributed under the License is distributed on an
- "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- KIND, either express or implied. See the License for the
- specific language governing permissions and limitations
- under the License.
+  This is a Blackbox. 
 *)
 
 open Arg
 open Thrift
-open Tutorial_types
-open Shared_types
+open Flowlog_types
 
+(* "Die, Bart, Die!" is German for "The, Bart, The!". -- Sideshow Bob *)
 exception Die;;
 let sod = function
     Some v -> v
   | None -> raise Die;;
 
-class calc_handler =
+class bb_handler =
 object (self)
-  inherit Calculator.iface
-  val log = Hashtbl.create 23
-  method ping  = Printf.printf "ping()\n" ; flush stdout
-  method add a b =
-    Printf.printf"add(%ld,%ld)\n" (sod a) (sod b); flush stdout ;
-    Int32.add (sod a) (sod b)
-  method calculate logid w =
-    let w = sod w in
-      Printf.printf "calculate(%ld,{%ld,%ld,%ld})\n" (sod logid) (Operation.to_i w#grab_op) w#grab_num1 w#grab_num2; flush stdout ;
-    let rv =
-      match w#grab_op with
-	  Operation.ADD ->
-	    Int32.add w#grab_num1 w#grab_num2
-	| Operation.SUBTRACT ->
-	    Int32.sub w#grab_num1 w#grab_num2
-	| Operation.MULTIPLY ->
-	    Int32.mul w#grab_num1 w#grab_num2
-	| Operation.DIVIDE ->
-	    if w#grab_num2 = Int32.zero then
-	      let io = new invalidOperation in
-		io#set_what (Operation.to_i w#grab_op) ;
-		io#set_why "Cannot divide by 0" ;
-		raise (InvalidOperation io)
-	    else
-	      Int32.div w#grab_num1 w#grab_num2 in
+  inherit BlackBox.iface
 
-    let ss = new sharedStruct in
-      ss#set_key (sod logid) ;
-    let buffer = Int32.to_string rv in
-      ss#set_value buffer ;
-      Hashtbl.add log (sod logid) ss ;
-      rv
+  method notifyMe notif = 
+    Printf.printf "notified()\n%!"
 
-  method zip =
-    Printf.printf "zip()\n"; flush stdout
-
-  method getStruct logid =
-    Printf.printf "getStruct(%ld)\n" (sod logid) ; flush stdout ;
-    Hashtbl.find log (sod logid)
-
+  method doQuery qry =
+    Printf.printf"handlequery\n%!";
+    (* need to return a QueryReply *)
+    let rep = new queryReply in
+      rep#set_result (Hashtbl.create 1);
+      rep
 end
 
-let doserver () =
-  let h = new calc_handler in
-  let proc = new Calculator.processor h in
+let dobb () =
+  let h = new bb_handler in
+  let proc = new BlackBox.processor h in
   let port = 9090 in
   let pf = new TBinaryProtocol.factory in
   let server = new TThreadedServer.t
@@ -86,4 +44,6 @@ let doserver () =
     server#serve
 ;;
 
-doserver();;
+(* todo: SEND notifications---how? *)
+
+dobb();;
--- interpreter/thrift/testFL.ml
+++ interpreter/thrift/testFL.ml
@@ -1,27 +1,15 @@
 (*
- Licensed to the Apache Software Foundation (ASF) under one
- or more contributor license agreements. See the NOTICE file
- distributed with this work for additional information
- regarding copyright ownership. The ASF licenses this file
- to you under the Apache License, Version 2.0 (the
- "License"); you may not use this file except in compliance
- with the License. You may obtain a copy of the License at
+  Modified from ocaml tutorial by Tim
 
-   http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing,
- software distributed under the License is distributed on an
- "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- KIND, either express or implied. See the License for the
- specific language governing permissions and limitations
- under the License.
+  This is the FlowLog server for catching notifications,
+    sending queries, and sending notifications.
 *)
 
 open Arg
 open Thrift
-open Tutorial_types
-open Shared_types
+open Flowlog_types
 
+(* "Die, Bart, Die!" is German for "The, Bart, The!". -- Sideshow Bob *)
 exception Die;;
 let sod = function
     Some v -> v
@@ -30,25 +18,39 @@ let sod = function
 type connection = {
   trans : Transport.t ;
   proto : Thrift.Protocol.t;
-  calc : Calculator.client ;
+  bb : BlackBox.client ;
 }
 
+(* The ~ denotes a keyword argument *)
 let connect ~host port =
   let tx = new TSocket.t host port in
   let proto = new TBinaryProtocol.t tx in
-  let calc = new Calculator.client proto proto in
+  let bb = new BlackBox.client proto proto in
     tx#opn;
-    { trans = tx ; proto = proto; calc = calc }
+    { trans = tx ; proto = proto; bb = bb}
 ;;
 
-let doclient () =
+let dofl () =
   let cli = connect ~host:"127.0.0.1" 9090 in
   try
-    cli.calc#ping ;
-    Printf.printf "ping()\n" ; flush stdout ;
-    (let sum = cli.calc#add (Int32.of_int 1) (Int32.of_int 1) in
-       Printf.printf "1+1=%ld\n" sum ;
-       flush stdout) ;
+    Printf.printf "sending a notification\n%!"; 
+    let notif = new notification in
+      notif#set_notificationType "test";
+      notif#set_values (Hashtbl.create 1);
+      cli.bb#notifyMe notif;
+    
+    Printf.printf "notification sent\n%!"; 
+
+    Printf.printf "querying\n%!";
+    let qry = new query in
+      qry#set_relName "testRel";
+      qry#set_arguments ["1";"2"]; 
+      let qresult = cli.bb#doQuery qry in            
+        (*Printf.printf "result=%s\n%!" qresult#get_result;  *)
+        Printf.printf "result received\n%!";
+    Printf.printf "query done\n%!";
+
+(*
     (let w = new work in
        w#set_op Operation.DIVIDE ;
        w#set_num1 (Int32.of_int 1) ;
@@ -67,8 +69,16 @@ let doclient () =
     (let ss = cli.calc#getStruct (Int32.of_int 1) in
        Printf.printf "Check log: %s\n" ss#grab_value ; flush stdout) ;
     cli.trans#close
+
+*)
+
   with Transport.E (_,what) ->
     Printf.printf "ERROR: %s\n" what ; flush stdout
+
 ;;
 
-doclient();;
+(* todo: receive notifications. this was adapted from client tutorial, so 
+    it presently just sends notifs and queries. *)
+
+(* todo: REGISTRATION? *)
+dofl();;
