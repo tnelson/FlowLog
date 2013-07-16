79a26675075a561d588ef37b7b623fa781d2b826
Michael Scheer <mgscheer@gmail.com>
2013-07-16 10:10:59 -0400

    fixed mli

--- interpreter/Xsb_Communication.mli
+++ interpreter/Xsb_Communication.mli
@@ -5,6 +5,7 @@ module Xsb : sig
 	val send_assert : string -> string;;
 	val send_query : string -> int -> (string list) list;;
 	val halt_xsb : unit -> unit;;
+	val debug_print_listings : unit -> unit;;
 end
 
 module Communication : sig
