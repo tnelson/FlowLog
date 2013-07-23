be73e4a38ba9c02cecfb29a5019a77567bd666b7
Michael Scheer <mgscheer@gmail.com>
2013-07-23 14:18:29 -0400

    debugging

--- interpreter/_build/Evaluation.mli
+++
@@ -1,9 +0,0 @@
-open Types;;
-open Packet;;
-open OxPlatform;;
-open OpenFlow0x01_Core;;
-open OpenFlow0x01;;
-
-module Evaluation : sig
-	val respond_to_notification : Types.term -> Types.program -> unit;;
-end
