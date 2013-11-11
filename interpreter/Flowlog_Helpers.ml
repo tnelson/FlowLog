12262844ccd95f6147cf452ad101b774a6504d72
Tim Nelson <tn@cs.wpi.edu>
2013-11-11 15:18:33 -0500

    removed unused, wrong function. use is_packet_in_table instead.

--- interpreter/Flowlog_Helpers.ml
+++ interpreter/Flowlog_Helpers.ml
@@ -132,9 +132,6 @@ let rec uses_relation (goal_modname: string) (goal_relname: string) (f: formula)
 		| FAtom(modname, relname, tlargs) ->
 			relname = goal_relname && modname = goal_modname;;
 
-let is_packet_triggered_clause (cl: clause): bool =
-  uses_relation "" packet_in_relname cl.body;;
-
 let product_of_lists lst1 lst2 = 
   List.concat (List.map (fun e1 -> List.map (fun e2 -> (e1,e2)) lst2) lst1);;
 
