c119dd5ab02af2912913b7f8d68d115c1daf7da3
Andrew Ferguson <adferguson@gmail.com>
2014-01-18 23:55:12 -0500

    first stage of converting to 1 tr,acl per router
    - protobuf network description updated
    - mininet script updated
    - sample config updated (and debugged)
    - templates for IOS-generated code updated (except ACLs)

--- mininet/FlowlogMN.py
+++ mininet/FlowlogMN.py
@@ -28,6 +28,10 @@ SWITCHDEF =  'ovshtb'
 SWITCHES = { 'ovshtb': OVSHtbQosSwitch,
              'user': UserSwitch }
 
+CREATE_EDGE_DEF = 'true'
+CREATE_EDGE = { 'true': True,
+                'false': False }
+
 class FlowlogDemo(object):
     "Run a Flowlog demo."
 
@@ -41,6 +45,7 @@ class FlowlogDemo(object):
       self.args = None
 
       self.subnetRootSwitch = None
+      self.subnetRootSwitchBaseDpid = None
       self.subnetHostLastIP = {}
       self.globalHostCount = 0
       self.globalPeerCount = 0
@@ -51,9 +56,12 @@ class FlowlogDemo(object):
       lg.setLogLevel('info')
       self.runDemo()
 
+    def setSubnetRootSwitchBaseDpid(self, routers):
+      self.subnetRootSwitchBaseDpid = int(routers.subnet_base_dpid, 16)
+
     def nextSubnetRootSwitch(self, network):
       num = len(self.subnetRootSwitch) + 1
-      dpid = 0x3000000000000000 + num # TODO(adf): make configurable
+      dpid = self.subnetRootSwitchBaseDpid + num
 
       return network.addSwitch('rs' + str(num), dpid=hex(dpid)[2:]) # 0x is implicit
 
@@ -85,7 +93,24 @@ class FlowlogDemo(object):
         opts = OptionParser()
         addDictOption(opts, SWITCHES, SWITCHDEF, 'switch')
         addDictOption(opts, CONTROLLERS, CONTROLLERDEF, 'controller')
+        addDictOption(opts, CREATE_EDGE, CREATE_EDGE_DEF, 'create_edge')
         self.options, self.args = opts.parse_args()
+
+
+    # The router is attached to each subnet first via the dlDst translator.
+    # The translator is then attached to an ACL table.
+    # Then, we attach that ACL table to the subnet's "subnetRootSwitch";
+    # if no such switch exists, we create it.
+    #
+    # So, from the outermost switch (ACL), subnet 1's rootSwitch is at
+    # port 1; port 2 connects to port 1 on the translator. Then, port 2
+    # on the translator is attached to port 2 on the router, where the
+    # pipeline terminates (port 1 on the router is reserved for the NAT)
+    #
+    # In general, on the ACL switch and the Translator switch, subnet N
+    # is on port (2N-1) for incoming from hosts, and port (2N) for
+    # outgoing to the next switch in the pipeline.
+    # On the router, subnets are attached sequentially start at port 2.
 
     def buildRouter(self, network, r):
       r.name = r.name.encode('ascii', 'ignore')
@@ -95,28 +120,19 @@ class FlowlogDemo(object):
       nat = network.addSwitch(r.name + '-nat', dpid=r.nat_dpid)
       network.addLink(router, nat, **self.linkopts)
 
-      # Add the dlDst translators for each subnet
-      # The router is attached to each subnet first via a dlDst translator.
-      # The translator is then attached to an ACL table.
-      # Then, we attach that ACL table to the subnet's "subnetRootSwitch";
-      # if no such switch exists, we create it.
+      translator = network.addSwitch(r.name + '-tr', dpid=r.tr_dpid)
+      acl_table = network.addSwitch(r.name + '-acl', dpid=r.acl_dpid)
 
       for (i, s) in enumerate(r.subnets):
         s.gw = s.gw.encode('ascii', 'ignore')
 
-        translator = network.addSwitch(r.name + '-t' + str(i + 1),
-                                       dpid=s.tr_dpid)
-        network.addLink(router, translator, **self.linkopts)
-
-        acl_table = network.addSwitch(r.name + '-a' + str(i + 1),
-                                      dpid=s.acl_dpid)
-        network.addLink(translator, acl_table, **self.linkopts)
-
         srs = self.subnetRootSwitch[subnetStr(s.addr, s.mask)]
-        network.addLink(acl_table, srs, **self.linkopts)
+        network.addLink(srs, acl_table, **self.linkopts)
+        network.addLink(acl_table, translator, **self.linkopts)
+        network.addLink(translator, router, **self.linkopts)
 
         # if there's room for hosts, add an edge switch with some hosts!
-        if s.mask < 30:
+        if s.mask < 30 and self.options.create_edge:
           self.globalEdgeSwCount += 1
           edge_switch = network.addSwitch('s' + str(self.globalEdgeSwCount))
           network.addLink(srs, edge_switch)
@@ -200,6 +216,7 @@ class FlowlogDemo(object):
 
     def runDemo(self, host_cmd='/usr/sbin/sshd', host_cmd_opts='-D'):
         routers = self.readProtobuf()
+        self.setSubnetRootSwitchBaseDpid(routers)
 
         controller = customConstructor(CONTROLLERS, self.options.controller)
         switch = customConstructor(SWITCHES, self.options.switch)
--- mininet/routers_pb2.py
+++ mininet/routers_pb2.py
@@ -11,7 +11,7 @@ from google.protobuf import descriptor_pb2
 DESCRIPTOR = descriptor.FileDescriptor(
   name='routers.proto',
   package='flowlog',
-  serialized_pb='\n\rrouters.proto\x12\x07\x66lowlog\"S\n\x06Subnet\x12\x0c\n\x04\x61\x64\x64r\x18\x01 \x01(\t\x12\x0c\n\x04mask\x18\x02 \x01(\x05\x12\n\n\x02gw\x18\x03 \x01(\t\x12\x0f\n\x07tr_dpid\x18\x04 \x01(\t\x12\x10\n\x08\x61\x63l_dpid\x18\x05 \x01(\t\"%\n\x07Network\x12\x0c\n\x04\x61\x64\x64r\x18\x01 \x01(\t\x12\x0c\n\x04mask\x18\x02 \x01(\x05\"Q\n\x04Peer\x12\n\n\x02ip\x18\x01 \x01(\t\x12\x0c\n\x04mask\x18\x02 \x01(\x05\x12\x0b\n\x03mac\x18\x03 \x01(\t\x12\"\n\x08networks\x18\x04 \x03(\x0b\x32\x10.flowlog.Network\"{\n\x06Router\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x11\n\tself_dpid\x18\x02 \x01(\t\x12\x10\n\x08nat_dpid\x18\x03 \x01(\t\x12 \n\x07subnets\x18\x04 \x03(\x0b\x32\x0f.flowlog.Subnet\x12\x1c\n\x05peers\x18\x05 \x03(\x0b\x32\r.flowlog.Peer\"+\n\x07Routers\x12 \n\x07routers\x18\x01 \x03(\x0b\x32\x0f.flowlog.Router')
+  serialized_pb='\n\rrouters.proto\x12\x07\x66lowlog\"0\n\x06Subnet\x12\x0c\n\x04\x61\x64\x64r\x18\x01 \x01(\t\x12\x0c\n\x04mask\x18\x02 \x01(\x05\x12\n\n\x02gw\x18\x03 \x01(\t\"%\n\x07Network\x12\x0c\n\x04\x61\x64\x64r\x18\x01 \x01(\t\x12\x0c\n\x04mask\x18\x02 \x01(\x05\"Q\n\x04Peer\x12\n\n\x02ip\x18\x01 \x01(\t\x12\x0c\n\x04mask\x18\x02 \x01(\x05\x12\x0b\n\x03mac\x18\x03 \x01(\t\x12\"\n\x08networks\x18\x04 \x03(\x0b\x32\x10.flowlog.Network\"\x9e\x01\n\x06Router\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x11\n\tself_dpid\x18\x02 \x01(\t\x12\x10\n\x08nat_dpid\x18\x03 \x01(\t\x12\x0f\n\x07tr_dpid\x18\x06 \x01(\t\x12\x10\n\x08\x61\x63l_dpid\x18\x07 \x01(\t\x12 \n\x07subnets\x18\x04 \x03(\x0b\x32\x0f.flowlog.Subnet\x12\x1c\n\x05peers\x18\x05 \x03(\x0b\x32\r.flowlog.Peer\"E\n\x07Routers\x12 \n\x07routers\x18\x01 \x03(\x0b\x32\x0f.flowlog.Router\x12\x18\n\x10subnet_base_dpid\x18\x02 \x01(\t')
 
 
 
@@ -44,20 +44,6 @@ _SUBNET = descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
-    descriptor.FieldDescriptor(
-      name='tr_dpid', full_name='flowlog.Subnet.tr_dpid', index=3,
-      number=4, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=unicode("", "utf-8"),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None),
-    descriptor.FieldDescriptor(
-      name='acl_dpid', full_name='flowlog.Subnet.acl_dpid', index=4,
-      number=5, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=unicode("", "utf-8"),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None),
   ],
   extensions=[
   ],
@@ -68,7 +54,7 @@ _SUBNET = descriptor.Descriptor(
   is_extendable=False,
   extension_ranges=[],
   serialized_start=26,
-  serialized_end=109,
+  serialized_end=74,
 )
 
 
@@ -102,8 +88,8 @@ _NETWORK = descriptor.Descriptor(
   options=None,
   is_extendable=False,
   extension_ranges=[],
-  serialized_start=111,
-  serialized_end=148,
+  serialized_start=76,
+  serialized_end=113,
 )
 
 
@@ -151,8 +137,8 @@ _PEER = descriptor.Descriptor(
   options=None,
   is_extendable=False,
   extension_ranges=[],
-  serialized_start=150,
-  serialized_end=231,
+  serialized_start=115,
+  serialized_end=196,
 )
 
 
@@ -185,14 +171,28 @@ _ROUTER = descriptor.Descriptor(
       is_extension=False, extension_scope=None,
       options=None),
     descriptor.FieldDescriptor(
-      name='subnets', full_name='flowlog.Router.subnets', index=3,
+      name='tr_dpid', full_name='flowlog.Router.tr_dpid', index=3,
+      number=6, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=unicode("", "utf-8"),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    descriptor.FieldDescriptor(
+      name='acl_dpid', full_name='flowlog.Router.acl_dpid', index=4,
+      number=7, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=unicode("", "utf-8"),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    descriptor.FieldDescriptor(
+      name='subnets', full_name='flowlog.Router.subnets', index=5,
       number=4, type=11, cpp_type=10, label=3,
       has_default_value=False, default_value=[],
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     descriptor.FieldDescriptor(
-      name='peers', full_name='flowlog.Router.peers', index=4,
+      name='peers', full_name='flowlog.Router.peers', index=6,
       number=5, type=11, cpp_type=10, label=3,
       has_default_value=False, default_value=[],
       message_type=None, enum_type=None, containing_type=None,
@@ -207,8 +207,8 @@ _ROUTER = descriptor.Descriptor(
   options=None,
   is_extendable=False,
   extension_ranges=[],
-  serialized_start=233,
-  serialized_end=356,
+  serialized_start=199,
+  serialized_end=357,
 )
 
 
@@ -226,6 +226,13 @@ _ROUTERS = descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
+    descriptor.FieldDescriptor(
+      name='subnet_base_dpid', full_name='flowlog.Routers.subnet_base_dpid', index=1,
+      number=2, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=unicode("", "utf-8"),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
   ],
   extensions=[
   ],
@@ -235,8 +242,8 @@ _ROUTERS = descriptor.Descriptor(
   options=None,
   is_extendable=False,
   extension_ranges=[],
-  serialized_start=358,
-  serialized_end=401,
+  serialized_start=359,
+  serialized_end=428,
 )
 
 _PEER.fields_by_name['networks'].message_type = _NETWORK
