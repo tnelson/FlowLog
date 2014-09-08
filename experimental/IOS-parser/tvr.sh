8eb4bef10b15318923f786c20db1bb1e98e8f411
Tim Nelson <tn@cs.brown.edu>
2014-09-07 21:23:25 -0400

    add discovery.flg for testing

--- experimental/IOS-parser/tvr.sh
+++ experimental/IOS-parser/tvr.sh
@@ -1,1 +1,8 @@
-./ios2flowlog --path tests/vr1/ --conn conns.txt configA.txt configB.txt configC.txt configD.txt int.txt extracl.txt
+ ./ios2flowlog --path tests/vr1/ configA.txt configC.txt
+
+
+
+# triangle
+# ./ios2flowlog --path tests/vr1/ configA.txt configC.txt configD.txt
+
+#./ios2flowlog --path tests/vr1/ --conn conns.txt configA.txt configB.txt configC.txt configD.txt int.txt extracl.txt
