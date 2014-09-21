529ec407a5fde5fd03cc337e5f0bded0f7afa32d
Tim Nelson <tn@cs.brown.edu>
2014-09-21 19:49:27 -0400

    don't clobber ourselves with optimization

--- interpreter/Flowlog_Parse_Helpers.ml
+++ interpreter/Flowlog_Parse_Helpers.ml
@@ -712,7 +712,8 @@ let desugared_program_of_ast (ast: flowlog_ast) (filename : string): flowlog_pro
 
             (* Create and simplify clauses. Test for what can be compiled. Weaken as needed. *)
             let clauses = (fold_left (fun acc r -> (clauses_of_rule r) @ acc) [] the_rules) in
-            let simplified_clauses = (map simplify_clause clauses) in
+            let simplified_clauses_maybe_false = (map simplify_clause clauses) in
+            let simplified_clauses = (filter (fun cl -> cl.body <> FFalse) simplified_clauses_maybe_false) in
 
             (* pre-determine what can be fully compiled. pre-determine weakened versions of other packet-triggered clauses*)
             let can_fully_compile_simplified, weakened_cannot_compile_pt_clauses, not_fully_compiled_clauses,_ =
